742a14233fca447e7113fc0cb26996c05a769efb
Vladimir Zaigrajew <vladimirzaigrajew@gmail.com>
2022-09-02 15:58:45 +0200

    Created train pipeline

--- src/predict.py
+++ src/predict.py
@@ -4,23 +4,21 @@ from typing import List
 import numpy as np
 
 
-def custom_predict(y_prob: np.ndarray, threshold: int, index: int) -> np.ndarray:
+def custom_predict(y_prob: np.ndarray, threshold: float, index: int = 0) -> np.ndarray:
     """Custom predict function that defaults
     to an index if conditions are not met.
-
     Args:
-        y_prob (np.ndarray): Array with probabilities.
-        threshold (int): Threshold cutoff.
-        index (int): Index to return if conditions are not met.
-
+        y_prob (np.ndarray): Predicted probabilities
+        threshold (float): Minimum softmax score to predict majority class
+        index (int): Label index to use if custom conditions is not met.
     Returns:
-        np.ndarray: Array with custom condition predictions.
+        np.ndarray: Predicted label indices.
     """
     y_pred = [np.argmax(p) if max(p) > threshold else index for p in y_prob]
     return np.array(y_pred)
 
 
-def predict(input_data: np.ndarray, artifacts: dict) -> List[dict]:
+def predict(input_data: np.ndarray, artifacts: dict) -> List:
     """Predict smoke for input data.
 
     Args:
@@ -28,6 +26,18 @@ def predict(input_data: np.ndarray, artifacts: dict) -> List[dict]:
         artifacts (dict): Dictionary with artifacts.
 
     Returns:
-        List[dict]: List of dictionaries with predictions.
+        List: Predictions for input.
     """
-    pass
+    y_pred = custom_predict(
+        y_prob=artifacts["model"].predict_proba(input_data),
+        threshold=artifacts["args"].threshold,
+        index=artifacts["label_encoder"].class_to_index["other"],
+    )
+    predictions = [
+        {
+            "input_text": input_data[i],
+            "Smoke_detected": y_pred[i],
+        }
+        for i in range(len(y_pred))
+    ]
+    return predictions
